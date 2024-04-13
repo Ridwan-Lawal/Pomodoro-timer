b55aaee3224d533e613bcf2d38d8090792806108
Ridwan-Lawal <lawal6382@gmail.com>
2024-04-13 10:10:54 +0100

    commit

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -11,7 +11,7 @@ function Homepage() {
   const { dispatch } = useTimer();
 
   return (
-    <div className="flex flex-col items-center justify-around  min-h-screen border-2  ">
+    <div className="flex flex-col items-center justify-around  h-[97vh]   ">
       <div className="flex flex-col items-center ">
         <h1 className="font-bold text-[32px] text-lightblue pb-11">pomodoro</h1>
         <TimerTab />
